7d8f79c5b5ebcad753575f30feaf2c263729a4f5
Manan <manan_rana@hotmail.com>
2021-10-06 07:43:29 +1100

    improved consistency throughout the code and improved the table output

--- main.py
+++ main.py
@@ -41,49 +41,50 @@ def main(file):
             currency_array, to_array, notes_array, instantly_exchanged_array, transfer_total_array, 
             transfer_total_currency_array, transfer_total_fee_array, transfer_fee_currency_array,
             transfer_payment_method_array, transfer_id_array, coinbase_id_array]
+
         for line in contents:
             if '----------' in line:
                 next_transaction = True
-            if 'Account_name' in line:
+            if 'Account_name:' in line:
                 account_name_array.append(line.strip("Account_name:"))
-            if 'Timestamp' in line: 
+            if 'Timestamp:' in line: 
                 timestamp_array.append(line.strip("Timestamp:"))
-            if 'Balance' in line: 
+            if 'Balance:' in line: 
                 balance_array.append(line.strip("Balance:"))
-            if 'Amount' in line: 
+            if 'Amount:' in line: 
                 amount_array.append(line.strip("Amount:"))
-            if 'Currency' in line: 
+            if 'Currency:' in line: 
                 currency_array.append(line.strip("Currency:"))
-            if 'To' in line: 
+            if 'To:' in line: 
                 to_array.append(line.strip("To:"))
-            if 'Notes' in line: 
+            if 'Notes:' in line: 
                 notes_array.append(line.strip("Notes:"))
-            if 'Instantly_exchanged' in line: 
+            if 'Instantly_exchanged:' in line: 
                 instantly_exchanged_array.append(line.strip("Instantly_exchanged:"))
             if 'Transfer_total:' in line: 
                 transfer_total_array.append(line.strip("Transfer_total:"))
             if 'Transfer_total_currency:' in line: 
                 transfer_total_currency_array.append(line.strip("Transfer_total_currency:"))
-            if 'Transfer_total_fee' in line:
+            if 'Transfer_total_fee:' in line:
                 transfer_total_fee_array.append(line.strip("Transfer_total_fee:"))
-            if 'Transfer_fee_currency' in line:
+            if 'Transfer_fee_currency:' in line:
                 transfer_fee_currency_array.append(line.strip("Transfer_fee_currency:"))
-            if 'Transfer_payment_method' in line:
+            if 'Transfer_payment_method:' in line:
                 transfer_payment_method_array.append(line.strip("Transfer_payment_method:"))
-            if 'Transfer_id' in line: 
+            if 'Transfer_id:' in line: 
                 transfer_id_array.append(line.strip("Transfer_id:"))
-            if 'Coinbase_id' in line: 
+            if 'Coinbase_id:' in line: 
                 coinbase_id_array.append(line.strip("Coinbase_id:"))
             
             if next_transaction:
+                base_length = len(account_name_array)
                 for item in array_of_variables:
-                    base_length = len(account_name_array)
                     if len(item) < base_length:
                         item.append("")
                 next_transaction = False
 
         #removed notes and payment method to improve readability 
-        #'Notes': notes_array, 'Transfer_payment_method': transfer_payment_method_array,
+        #'Notes': notes_array, || 'Transfer_payment_method': transfer_payment_method_array,
         transactions_table = {
             'Account_name': account_name_array,
             'Timestamp': timestamp_array,
@@ -97,8 +98,7 @@ def main(file):
             'Transfer_fee_currency': transfer_fee_currency_array,
             
         }
-
-        print(tabulate(transactions_table, headers='keys'))
+        print(tabulate(transactions_table, headers='keys', tablefmt='fancy_grid', showindex=True))
 
         
         
